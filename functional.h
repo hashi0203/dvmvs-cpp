66e9ae44a3da9b15fb189ed5d65860bf0bb4a456
hashi0203 <n.hsmt1123@gmail.com>
2021-12-31 23:02:58 +0900

    fix bugs in grid_sample

--- functional.h
+++ functional.h
@@ -64,19 +64,17 @@ void grid_sample(const float image[channels][height][width],
     for (int j = 0; j < height; j++) for (int k = 0; k < width; k++) {
         const float x = (warping[j][k][0] + 1) * (width - 1) / 2.0; // maybe vice versa
         const float y = (warping[j][k][1] + 1) * (height - 1) / 2.0;
-        if (isnan(y) || y < 0 || height-1 < y || isnan(x) || x < 0 || width-1 < x) {
-            for (int i = 0; i < channels; i++) warped_image[i][j][k] = 0;
-        } else {
-            const int y_int = min(y, (float) height-2);
-            const int x_int = min(x, (float) width-2);
-            const int ys[2] = {y_int, y_int + 1};
-            const int xs[2] = {x_int, x_int + 1};
-            const float dys[2] = {y - ys[0], ys[1] - y};
-            const float dxs[2] = {x - xs[0], xs[1] - x};
-            for (int i = 0; i < channels; i++) {
-                warped_image[i][j][k] = 0;
-                for (int yi = 0; yi < 2; yi++) for (int xi = 0; xi < 2; xi++)
-                    warped_image[i][j][k] += dys[1-yi] * dxs[1-xi] * image[i][ys[yi]][xs[xi]];
+        const int y_int = floor(y);
+        const int x_int = floor(x);
+        const int ys[2] = {y_int, y_int + 1};
+        const int xs[2] = {x_int, x_int + 1};
+        const float dys[2] = {y - ys[0], ys[1] - y};
+        const float dxs[2] = {x - xs[0], xs[1] - x};
+        for (int i = 0; i < channels; i++) {
+            warped_image[i][j][k] = 0;
+            for (int yi = 0; yi < 2; yi++) for (int xi = 0; xi < 2; xi++) {
+                const float val = (ys[yi] < 0 || height-1 < ys[yi] || xs[xi] < 0 || width-1 < xs[xi]) ? 0 : image[i][ys[yi]][xs[xi]];
+                warped_image[i][j][k] += dys[1-yi] * dxs[1-xi] * val;
             }
         }
     }
