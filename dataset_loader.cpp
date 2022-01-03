b22e3ddded99f3cfb39868e02aa7441ac670eca3
hashi0203 <n.hsmt1123@gmail.com>
2022-01-03 12:23:31 +0900

    refactored

--- dataset_loader.cpp
+++ dataset_loader.cpp
@@ -10,12 +10,14 @@ void load_image(const string image_filename, float reference_image[org_image_hei
         reference_image[i][j][k] = rgb.data[540*3*i + 3*j + k];
 }
 
+
 void save_image(const string image_filename, float depth[test_image_height][test_image_width]) {
     cv::Mat gray(test_image_height, test_image_width, CV_8U);
     for (int i = 0; i < test_image_height; i++) for (int j = 0; j < test_image_width; j++)
         gray.data[test_image_width*i + j] = depth[i][j] * 150;
     cv::imwrite(image_filename, gray);
 }
+
 
 void PreprocessImage::apply_rgb(const float org_image[org_image_height][org_image_width][3], float image[3][test_image_height][test_image_width]) {
     float src[3][org_image_height][org_image_width];
@@ -28,6 +30,7 @@ void PreprocessImage::apply_rgb(const float org_image[org_image_height][org_imag
     }
 }
 
+
 void PreprocessImage::get_updated_intrinsics(float updated_intrinsics[3][3]) {
     updated_intrinsics[0][0] = fx;
     updated_intrinsics[0][1] = 0;
@@ -39,4 +42,3 @@ void PreprocessImage::get_updated_intrinsics(float updated_intrinsics[3][3]) {
     updated_intrinsics[2][1] = 0;
     updated_intrinsics[2][2] = 1;
 }
-
