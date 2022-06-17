c4fb9d09d2ce60b1e48b7cf8c61d78adf6affa95
hashi0203 <n.hsmt1123@gmail.com>
2022-06-17 10:00:22 +0900

    fix bugs in convert-images

--- converter/convert-images.py
+++ converter/convert-images.py
@@ -10,19 +10,19 @@ def convert():
     save_dir = os.path.dirname(os.path.abspath(__file__)) / Path("../images")
     os.makedirs(save_dir, exist_ok=True)
 
-    for i in range(20):
+    for i in range(100):
         image_file = image_dir / "%05d.png" % (i+3)
         image = cv2.cvtColor(cv2.imread(image_file), cv2.COLOR_BGR2RGB).transpose(2, 0, 1)
         assert image.shape == (3, 360, 540)
 
-        for v in image.reshape(-1):
-            print(struct.pack('B', v))
-            print(v)
-            break
+        # for v in image.reshape(-1):
+        #     print(struct.pack('B', v))
+        #     print(v)
+        #     break
 
-        # with open(save_dir / "%05d" % (i+3), 'wb') as f:
-        #     for v in image.reshape(-1):
-        #         f.write(struct.pack('B', v))
+        with open(save_dir / "%05d" % (i+3), 'wb') as f:
+            for v in image.reshape(-1):
+                f.write(struct.pack('B', v))
 
 
 if __name__ == '__main__':
